b9f3e05cff5c8f93f7aadbfa4f1121ed0efcc355
arigos <arigos@ARIGOS1>
2017-04-17 19:21:27 -0300

    Nuevo

--- Presentacion/Presentacion/Scripts/Controllers/SolicitudTarjetaController.js
+++ Presentacion/Presentacion/Scripts/Controllers/SolicitudTarjetaController.js
@@ -1,5 +1,19 @@
 ﻿angular.module('app').controller("SolicitudTarjetaController", ["$scope", "$location", function ($scope, $location) {
 
     $scope.Titulo = "Datos del nuevo cliente, solicitud nueva tarjeta.";
+    $scope.estados = ["Soltero", "Casado", "Divorciado","Otro"];
+
+    $scope.verificarEstadoCivil = function () {
+        if ($scope.estadoSeleccionado == "2") {
+            $scope.datosConyuge = true;
+        } else {
+            $scope.datosConyuge = false;
+        }
+    }
+  
+    var ProcesarSolicitud = function () {
+        $scope.Procesando = true;
+        $scope.Tarea = "Obteniendo informacion del veraz";
+    }
 
 }])
